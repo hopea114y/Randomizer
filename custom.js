272e64c39ecfd990d542dbd13803a8807bacd283
hopea114y <serhioalfa@mail.ru>
2017-10-27 16:57:17 +0300

    The simplest check for a positive integer is implemented

--- custom.js
+++ custom.js
@@ -1,5 +1,7 @@
 window.onload = function() {
-	var maxNumber = +prompt('Enter a last participating number', '999');
+	do {
+		var maxNumber = +prompt('Enter a last participating positive integer', '999');
+	} while (maxNumber < 0 || maxNumber == null || maxNumber == NaN);
 	var res = document.querySelector('.container');
 
 	function randomNumber(min, max) {
