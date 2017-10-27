3ecf045e76920c7ecb6597c55e6c6c7afb7bb401
hopea114y <serhioalfa@mail.ru>
2017-10-27 19:36:50 +0300

    Random iterations added

--- custom.js
+++ custom.js
@@ -1,11 +1,17 @@
 window.onload = function() {
 	do {
-		var maxNumber = +prompt('Enter a last participating positive integer', '999');
+	var maxNumber = +prompt('Enter a last participating positive integer', '999');
 	} while (maxNumber < 0 || maxNumber == null || maxNumber == NaN);
 	var res = document.querySelector('.container');
+
+	for(var i = 0; i < 15; i++) {
+		setTimeout(function(i) {
+			res.innerHTML = randomNumber(1, maxNumber);
+		}, 500 * i + 1, i);
+	}
 
 	function randomNumber(min, max) {
 		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
-	res.innerHTML = randomNumber(1, maxNumber);
+	
 }
